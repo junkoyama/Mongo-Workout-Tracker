760f4e9ef6c5cddea8f50b56a7f20ebccd1877de
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-07 15:23:58 -0500

    Added more code to set up server

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -7,7 +7,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-mongoose.connect("mongodb://localhost/workout", {
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
