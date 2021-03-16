bb5c4c875438b4ef14585ae9a69c3981a008d71f
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-15 22:57:55 -0400

    Set up GET, POST, PUT requests and worked together with tutor to successfully run

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,1 +1,78 @@
-const db = require('./models');
+const router = require("express").Router();
+const Workout = require("../models/Workout");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+  Workout.findByIdAndUpdate(
+    params.id,
+    {
+      $push: {
+        exercises: body,
+      },
+    },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", ({ query }, res) => {
+  // Workout.find({}).then((dbWorkout) => {
+  //   console.log(dbWorkout);
+  //   res.json((dbWorkout) ?? [])
+  // })
+  //   .catch((err) => {
+  //     res.json(err);
+  //   });
+
+  Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: "$exercises.duration",
+        },
+      },
+    },
+  ])
+    .then((dbWorkout) => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
