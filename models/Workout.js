05d6cdcad85384de41877247abb21d545450db26
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-15 23:21:56 -0400

    Imported mongoose and models folder

---
+++ models/Workout.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: "Exercise type is required"
+      },
+      name: {
+        type: String,
+        required: "Exercise name is required"
+      },
+      duration: {
+        type: Number,
+        required: "Exercise duration is required"
+      },      
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      }
+    }]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
