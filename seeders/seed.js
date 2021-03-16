31837de7523f9cd5cd15b04feae8410e180461bf
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-15 23:23:23 -0400

    Imported mongoose and models folder

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,3 +1,11 @@
+const mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
 const workoutSeed = [
   {
     day: new Date().setDate(new Date().getDate()-10),
